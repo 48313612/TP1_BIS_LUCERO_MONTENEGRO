a78af802e564c26daf0a47f7bd448cae856a10d4
MARIA EUGENIA LUCERO <48313612@ort>
2025-03-20 12:11:47 -0300

    js

--- js/script.js
+++ js/script.js
@@ -3,4 +3,29 @@ function validarForm()
     let notaMatematica = document.getElementById('notaMatematica').value;
     let notaLengua = document.getElementById('notaLengua').value;
     let notaEFSI = document.getElementById('notaEFSI').value;
+    
+    //VALIDAR 
+    let notaMatematicaValida = validacion(notaMatematica);
+    let notaLenguaValida = validacion(notaLengua);
+    let notaEFSIValida = validacion(notaEFSI);
+
+    let mensaje = " ";
+    if(!notaMatematicaValida || !notaEFSIValida || !notaLenguaValida )
+    {
+        mensaje = "Error. Debe ser un número entre 1 y 10."
+    }
+}
+
+function validacion(datoAvalidar)
+{
+    const tieneLetra = /(?:[A-Z])/.test(datoAvalidar) || /(?:[a-z])/.test(datoAvalidar);
+    const tieneNum = /(?:\d)/.test(datoAvalidar);
+    let esValido = false;
+
+    if(datoAvalidar <= 10 && datoAvalidar >= 1 && tieneNum && !tieneLetra)
+    {
+        esValido = true;
+    }
+
+    return esValido;
 }
