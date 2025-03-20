6fe104b7ce8d99d294ff7ee8aaf95cf6c4c2bbc2
MARIA EUGENIA LUCERO <48313612@ort>
2025-03-20 11:37:03 -0300

    upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function validarForm()
+{
+    let notaMatematica = document.getElementById('notaMatematica').value;
+    let notaLengua = document.getElementById('notaLengua').value;
+    let notaEFSI = document.getElementById('notaEFSI').value;
+}
